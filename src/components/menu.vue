1773f7d66f8c88e6b3a36f889ac2537f53576456
niyongwei <1500920628@qq.com>
2021-07-22 17:20:52 +0800

    优化样式

--- src/components/menu.vue
+++ src/components/menu.vue
@@ -1,17 +1,29 @@
 <template>
   <div class="menu-content">
     <el-menu default-active="2" class="el-menu-vertical-demo" background-color="#545c64" text-color="#fff" active-text-color="#ffd04b">
-      <el-submenu index="1">
+      <el-menu-item index="1">
+        <router-link to="/home">
+          <i class="el-icon-menu"></i>
+          <span>
+            首页
+          </span>
+        </router-link>
+      </el-menu-item>
+      <el-submenu index="2">
         <template slot="title">
           <i class="el-icon-location"></i>
-          <span>导航一</span>
+          <span>第一天</span>
         </template>
         <el-menu-item-group>
-          <el-menu-item index="1-1">选项1</el-menu-item>
-          <el-menu-item index="1-2">选项2</el-menu-item>
+          <router-link to="/todo">
+            <el-menu-item index="1-1">
+              练习1-todo
+            </el-menu-item>
+          </router-link>
+          <el-menu-item index="1-2">练习2</el-menu-item>
         </el-menu-item-group>
       </el-submenu>
-      <el-menu-item index="2">
+      <el-menu-item index="3">
         <i class="el-icon-menu"></i>
         <span slot="title">导航二</span>
       </el-menu-item>
@@ -26,4 +38,9 @@ export default {
 </script>
 
 <style scoped lang="scss">
+a {
+  display: block;
+  color: #fff;
+  text-decoration: none;
+}
 </style>
